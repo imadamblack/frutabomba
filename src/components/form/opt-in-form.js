ab24e449b54f4192b5cb7a037fdbd3b712087d0c
Adam Black <fernando@notoriovs.com>
2025-02-19 18:25:56 -0600

    UPD: opt-in form added 'pos' and 'commitment'

--- src/components/form/opt-in-form.js
+++ src/components/form/opt-in-form.js
@@ -1,26 +1,30 @@
 import Link from 'next/link';
 import { info } from '../../../info';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { useRouter } from 'next/router';
 import { getCookie, setCookie } from 'cookies-next';
 import { useState } from 'react';
 import { restrictNumber, emailRegExp, cleanPhone } from '../../utils/formValidators';
 import fbEvent from '../../services/fbEvents';
+import { Radio, Select } from './formAtoms';
 
 export default function OptInForm({lastClick = ''}) {
   const [sending, setSending] = useState(false);
   const router = useRouter();
+  const methods = useForm({mode: 'all'});
   const {
     register,
     handleSubmit,
     formState: {errors},
-  } = useForm();
+    watch
+  } = methods;
 
   const onSubmit = (data) => {
     setSending(true);
     data.phone = cleanPhone(data.phone);
     data.origin = 'Notoriovs Landing';
     data.lastClick = lastClick;
+    data.city = data.city === 'otro' ? data.cityOther : data.city;
 
     const _fbc = getCookie('_fbc');
     const _fbp = getCookie('_fbp');
@@ -48,48 +52,80 @@ export default function OptInForm({lastClick = ''}) {
   };
 
   return (
-    <form className="flex flex-col w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register(
-          'fullName',
-          {
-            required: true,
-          },
-        )}
-        className={errors.fullName && '!bg-red-200'}
-        placeholder="Tu nombre"/>
-      <input
-        {...register(
-          'phone',
-          {required: true, maxLength: 10, minLength: 10},
-        )}
-        className={errors.phone && '!bg-red-200'}
-        onKeyDown={restrictNumber}
-        placeholder="Teléfono de WhatsApp"/>
-      <input
-        {...register(
-          'city',
-          {
-            required: true,
-          },
-        )}
-        className={errors.city && '!bg-red-200'}
-        placeholder="Tu ciudad"/>
+    <FormProvider {...methods}>
+      <form className="flex flex-col w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register(
+            'fullName',
+            {
+              required: true,
+            },
+          )}
+          className={errors.fullName && '!bg-red-200'}
+          placeholder="Tu nombre"/>
+        <input
+          {...register(
+            'phone',
+            {required: true, maxLength: 10, minLength: 10},
+          )}
+          className={errors.phone && '!bg-red-200'}
+          onKeyDown={restrictNumber}
+          placeholder="Teléfono de WhatsApp"/>
+        <Select
+          name="city"
+          placeholder="Tu ciudad"
+          inputOptions={{required: true}}
+          options={[
+            {value: 'cdmx', name: 'CDMX'},
+            {value: 'ensenada', name: 'Ensenada'},
+            {value: 'hermosillo', name: 'Hermosillo'},
+            {value: 'mexicali', name: 'Mexicali'},
+            {value: 'queretaro', name: 'Querétaro'},
+            {value: 'rosarito', name: 'Rosarito'},
+            {value: 'tijuana', name: 'Tijuana'},
+            {value: 'otro', name: 'Otra'},
+          ]}
+        />
+        {watch('city') === 'otro' && <input {...register('cityOther',{required: true})} className={errors.cityOther && '!bg-red-200'} placeholder="Cuál ciudad?"/>}
 
-      <button
-        disabled={sending}
-        className={`w-full ${sending ? '!bg-transparent' : 'hover:!bg-brand-3'}`}
-      >{
-        !sending
-          ? 'Enviar WhatsApp →'
-          : <span className="material-symbols-outlined animate-spin">progress_activity</span>
-      }</button>
+        <Select
+          name="pos"
+          placeholder="Cómo piensas distribuir Fruta Bomba?"
+          inputOptions={{required: true}}
+          options={[
+            {value: 'tienda-fisica', name: 'Mi tienda física'},
+            {value: 'online', name: 'En línea'},
+            {value: 'cambaceo', name: 'Cambaceo'},
+            {value: 'otro', name: 'Otro'},
+          ]}
+        />
+        <div className="bg-white p-8 rounded-xl">
+          <p>Estás dispuesto a realizar un pedido mínimo?</p>
+          <Radio
+            name="commitment"
+            inputOptions={{required: true}}
+            options={[
+              {value: 'si', label: 'Sí, claro!'},
+              {value: 'no', label: 'No creo'},
+              {value: 'sample', label: 'Primero una muestra'},
+            ]}
+          />
+        </div>
+        <button
+          disabled={sending}
+          className={`w-full ${sending ? '!bg-transparent' : 'hover:!bg-brand-3'}`}
+        >{
+          !sending
+            ? 'Enviar WhatsApp →'
+            : <span className="material-symbols-outlined animate-spin">progress_activity</span>
+        }</button>
 
-      <div className="mt-4">
-        <p className="-ft-3 text-center text-white">Al dar clic aceptas nuestra&nbsp;
-          <Link href={info.privacyNotice}>política de privacidad</Link>
-        </p>
-      </div>
-    </form>
+        <div className="mt-4">
+          <p className="-ft-3 text-center text-white">Al dar clic aceptas nuestra&nbsp;
+            <Link href={info.privacyNotice}>política de privacidad</Link>
+          </p>
+        </div>
+      </form>
+    </FormProvider>
   );
 }
